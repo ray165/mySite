2e55686d856f8cde33e32de90159b46f457ff417
Raymond <70726900+ray165@users.noreply.github.com>
2021-06-05 17:54:49 -0700

    add swap subtitle

--- client/src/subcomp/home.js
+++ client/src/subcomp/home.js
@@ -1,32 +1,36 @@
-import React, { useState, useRef, useEffect } from 'react';
-import Typography from '@material-ui/core/Typography';
+import React, { useState, useRef, useEffect } from "react";
+import Typography from "@material-ui/core/Typography";
 
 export default function Home() {
-    const [fade, setFade] = useState(true)
-    const text = useRef('')
-    
+  const [fade, setFade] = useState(true);
+  const counter = useRef(0);
+  const [index, setIndex] = useState(0);
+  const descriptionArray = ["SFU Business", "Aspiring Software Developer", "BCIT Programming", "Voted Best Developer By My Mom"]
 
-    useEffect(() => {
-        var descriptionArray = ["SFU Business", "Aspiring Software Developer", "BCIT Programming", "Voted Best Developer By My Mom"]
-        let counter = 0
+  useEffect(() => {
+      
+      const looper = setInterval(() => {
+          counter.current++
+          if (counter.current === descriptionArray.length) {
+            counter.current = 0
+          }
+          setIndex(counter.current)
+      }, 2000)
+      return () => {
+          clearInterval(looper)
+        //   setFade(false)
+      }
+  }, [index])
 
-        const looper = setInterval(function(){
-            text.current = descriptionArray[counter]
-            counter++;
-            if (counter === descriptionArray.length) {
-                counter = 0;
-            }
-        }, 3500)
-        return () => {
-            clearInterval(looper)
-            setFade(false)
-        }
-    }, [fade])
+  return (
+    <>
+      <Typography variant="h2" component="h1">
+        Raymond Wong
+      </Typography>
+      <Typography >
+        {descriptionArray[index]}
+      </Typography>
 
-    return(
-        <>
-            <Typography variant="h2" component="h1">Raymond Wong</Typography>
-            <Typography>{text.current}</Typography>
-        </>
-    )
+    </>
+  );
 }
